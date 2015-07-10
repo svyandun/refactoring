d4740e5b07ab76dfb2d400763eca42430f39cf8c
Sebastián Yandún <svyandun@gmail.com>
2015-07-09 20:26:33 -0500

    Removing Temps

--- src/Customer.php
+++ src/Customer.php
@@ -23,19 +23,34 @@ class Customer
 
     public function statement()
     {
-        $totalAmount = 0;
-        $frequentRenterPoints = 0;
         $result = "Rental Record for " . $this->getName() . "\n";
         foreach ($this->rentals as $rental) {
-            $frequentRenterPoints += $rental->getFrequentRenterPoints($rental);
-
             // show figures for this rental
             $result .= "\t" . $rental->getMovie()->getTitle() . "\t" . $rental->getCharge() . "\n";
-            $totalAmount += $rental->getCharge();
         }
         // add footer lines
-        $result .= "Amount owed is " . $totalAmount . "\n";
-        $result .= "You earned " . $frequentRenterPoints . " frequent renter points";
+        $result .= "Amount owed is " . $this->getTotalCharge() . "\n";
+        $result .= "You earned " . $this->getTotalFrequentRenterPoints() . " frequent renter points";
+
+        return $result;
+    }
+
+    private function getTotalCharge()
+    {
+        $result = 0;
+        foreach ($this->rentals as $rental) {
+            $result += $rental->getCharge();
+        }
+
+        return $result;
+    }
+
+    private function getTotalFrequentRenterPoints()
+    {
+        $result = 0;
+        foreach ($this->rentals as $rental) {
+            $result += $rental->getFrequentRenterPoints();
+        }
 
         return $result;
     }
