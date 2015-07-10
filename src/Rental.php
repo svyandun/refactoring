a2622aee0faaa263a9ae2cd18ea9aae3b79f3a59
Sebastián Yandún <svyandun@gmail.com>
2015-07-09 19:54:52 -0500

    Move Method

--- src/Rental.php
+++ src/Rental.php
@@ -15,6 +15,30 @@ class Rental
         $this->daysRented = $daysRented;
     }
 
+    public function getCharge()
+    {
+        $result = 0;
+        switch ($this->getMovie()->getPriceCode()) {
+            case Movie::REGULAR:
+                $result += 2;
+                if ($this->getDaysRented() > 2) {
+                    $result += ($this->getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie::NEW_RELEASE:
+                $result += $this->getDaysRented() * 3;
+                break;
+            case Movie::CHILDRENS:
+                $result += 1.5;
+                if ($this->getDaysRented() > 3) {
+                    $result += ($this->getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+
+        return $result;
+    }
+
     /**
      * @return Movie
      */
