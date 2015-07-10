1bd75ebd607e309a34098502737b3d0efd1e288a
Sebastián Yandún <svyandun@gmail.com>
2015-07-09 20:42:45 -0500

    Move Method

--- src/Movie.php
+++ src/Movie.php
@@ -16,6 +16,38 @@ class Movie
         $this->priceCode = $priceCode;
     }
 
+    public function getCharge($daysRented)
+    {
+        $result = 0;
+        switch ($this->getPriceCode()) {
+            case Movie::REGULAR:
+                $result += 2;
+                if ($daysRented > 2) {
+                    $result += ($daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie::NEW_RELEASE:
+                $result += $daysRented * 3;
+                break;
+            case Movie::CHILDRENS:
+                $result += 1.5;
+                if ($daysRented > 3) {
+                    $result += ($daysRented - 3) * 1.5;
+                }
+                break;
+        }
+
+        return $result;
+    }
+
+    public function getFrequentRenterPoints($daysRented)
+    {
+        if ($this->getPriceCode() == Movie::NEW_RELEASE && $daysRented > 1) {
+            return 2;
+        }
+        return 1;
+    }
+
     public function getTitle()
     {
         return $this->title;
--- src/Rental.php
+++ src/Rental.php
@@ -17,36 +17,12 @@ class Rental
 
     public function getCharge()
     {
-        $result = 0;
-        switch ($this->getMovie()->getPriceCode()) {
-            case Movie::REGULAR:
-                $result += 2;
-                if ($this->getDaysRented() > 2) {
-                    $result += ($this->getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie::NEW_RELEASE:
-                $result += $this->getDaysRented() * 3;
-                break;
-            case Movie::CHILDRENS:
-                $result += 1.5;
-                if ($this->getDaysRented() > 3) {
-                    $result += ($this->getDaysRented() - 3) * 1.5;
-                }
-                break;
-        }
-
-        return $result;
+        return $this->movie->getCharge($this->daysRented);
     }
 
     public function getFrequentRenterPoints()
     {
-        if ($this->getMovie()->getPriceCode() == Movie::NEW_RELEASE &&
-            $this->getDaysRented() > 1
-        ) {
-            return 2;
-        }
-        return 1;
+        return $this->movie->getFrequentRenterPoints($this->daysRented);
     }
 
     /**
