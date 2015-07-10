198b556063dee5737adeb71437cc8fa94a09f726
Sebastián Yandún <svyandun@gmail.com>
2015-07-09 21:29:10 -0500

    Move Method

--- src/Price.php
+++ src/Price.php
@@ -4,4 +4,28 @@ namespace Refactoring;
 abstract class Price
 {
     abstract public function getPriceCode();
+
+    public function getCharge($daysRented)
+    {
+        $result = 0;
+        switch ($this->getPriceCode()) {
+            case Movie::REGULAR:
+                $result += 2;
+                if ($daysRented > 2) {
+                    $result += ($daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie::NEW_RELEASE:
+                $result += $daysRented * 3;
+                break;
+            case Movie::CHILDRENS:
+                $result += 1.5;
+                if ($daysRented > 3) {
+                    $result += ($daysRented - 3) * 1.5;
+                }
+                break;
+        }
+
+        return $result;
+    }
 }
